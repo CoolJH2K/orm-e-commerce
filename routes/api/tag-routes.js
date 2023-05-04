9048e80c04b3c1a971abef6ba95a3a06d3919e2a
Janna Harmon <harmonjanna00@gmail.com>
2023-05-04 19:41:49 -0400

    Updated tag-routes.js file.

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -1,28 +1,48 @@
 const router = require('express').Router();
 const { Tag, Product, ProductTag } = require('../../models');
 
-// The `/api/tags` endpoint
-
+// GET all tags
 router.get('/', (req, res) => {
   // find all tags
   // be sure to include its associated Product data
 });
 
+// GET a single tag
 router.get('/:id', (req, res) => {
-  // find a single tag by its `id`
   // be sure to include its associated Product data
 });
 
-router.post('/', (req, res) => {
-  // create a new tag
+// CREATE a new tag
+router.post('/', async (req, res) => {
+  try {
+    const tagData = await Tag.create(req.body);
+    res.status(200).json(tagData);
+  } catch (err) {
+    res.status(400).json(err);
+  }
 });
 
+// UPDATE a tag's name
 router.put('/:id', (req, res) => {
-  // update a tag's name by its `id` value
+  
 });
 
-router.delete('/:id', (req, res) => {
-  // delete on tag by its `id` value
+// DELETE a tag
+router.delete('/:id', async (req, res) => {
+  try {
+    const tagData = await Tag.destroy({
+      where: {
+        id: req.params.id
+      }
+    });
+    if (!tagData) {
+      res.status(404).json({message: "No tag found with this ID."});
+      return;
+    }
+    res.status(200).json(tagData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 module.exports = router;
