3cf942a646666de4ee3e6b91a9154e7f18357195
Janna Harmon <harmonjanna00@gmail.com>
2023-05-04 19:05:26 -0400

    Fixed an error in product-routes.js.

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -22,7 +22,14 @@ router.get('/:id', async (req, res) => {
   try {
     const productData = await Product.findByPk(req.params.id, {
       include: [{model: Category, through: Tag, as: "products"}]
-    })
+    });
+    if (!productData) {
+      res.status(404).json({message: "No product found with this ID."});
+      return;
+    }
+    res.status(200).json(productData);
+  } catch (err) {
+    res.status(500).json(err);
   }
 });
 
