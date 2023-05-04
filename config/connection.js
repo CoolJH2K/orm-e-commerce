12a1123a69f776d6d428f30234d4a3746f62339a
Janna Harmon <harmonjanna00@gmail.com>
2023-05-03 21:39:12 -0400

    Updated the connection.js file.

--- config/connection.js
+++ config/connection.js
@@ -1,15 +1,23 @@
+// Require dotnev and sequelize
 require('dotenv').config();
-
 const Sequelize = require('sequelize');
 
-const sequelize = process.env.JAWSDB_URL
-  ? new Sequelize(process.env.JAWSDB_URL)
-  : new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
-      host: 'localhost',
-      dialect: 'mysql',
-      dialectOptions: {
-        decimalNumbers: true,
-      },
-    });
+let sequelize;
+
+// This will let the models connect to the database in MySQL
+if (process.env.JAWSDB_URL) {
+  sequelize = new Sequelize(process.env.JAWSDB_URL);
+} else {
+  sequelize = new Sequelize(
+    process.env.DV_NAME,
+    process.env.DV_USER,
+    process.env.DV_PASSWORD,
+    {
+      host: "localhost",
+      dialect: "mysql",
+      port: 3306
+    }
+  );
+}
 
 module.exports = sequelize;
