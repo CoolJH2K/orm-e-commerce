36ab663accccdbb933fd9db6ad9106eab289fcae
Janna Harmon <harmonjanna00@gmail.com>
2023-05-04 20:55:32 -0400

    Updated category-routes.js file.

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -17,7 +17,7 @@ router.get('/:id', async (req, res) => {
   // be sure to include its associated Products
   try {
     const categoryData = await Category.findByPk(req.params,id, {
-      include: [{model: Product, through: Tag, as: "product_id"}]
+      include: [{model: Product}]
     });
     if (!categoryData) {
       res.status(404).json({message: "No category with this ID."});
@@ -41,7 +41,20 @@ router.post('/', async (req, res) => {
 
 // UPDATE a category
 router.put('/:id', async (req, res) => {
-  
+  try {
+    const categoryData = await Category.update(req.body, {
+      where: {
+        id: req.params.id
+      }
+    });
+    if (!categoryData) {
+      res.status(404).json({message: "No category with this ID."});
+      return;
+    }
+    res.status(200).json(categoryData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 // DELETE a category
